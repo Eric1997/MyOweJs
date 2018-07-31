6dc4e9520e07ed475f23b2d033bc121d0b1172b0
Eric1997 <1556022882@qq.com>
2018-07-31 21:34:40 +0800

    add two function

--- Own-Es6.js
+++ Own-Es6.js
@@ -4,7 +4,43 @@
 用ES6语法重写
 会定时增添其他的方法,同时会不断改进类库的质量
  */
+import 'whateg-fetch';
+import 'es6-promise';
 
-let EventHandle = {
+let Handle = {
 	getEvent: () => event ? event : window.event;
+	get:(url)=>{
+		let result=fetch(url,{
+			credenttials:'include',
+			headers:{
+				'Accept':'application/json,text/plain,*/*'
+			}
+		});
+		return result;
+	}
+	objtoparams:(obj)=>{
+		let result='';
+		let item;
+		for(item in obj){
+			result+='&'+item+'='+encodeURLComponent(obj[item]);
+		}
+		if(result){
+			result=result.slice(1);
+		}
+		return result;
+	}
+	post:(url,obj)=>{
+		let result=fetch(url,{
+			method:'POST',
+			credentials:'include',
+			headers:{
+				'Accept':'application/json,text/plain,*/*',
+				'Content-Type':'application/x-www-form-urlencoded'
+			}
+			body:objtoparams(obj)
+		});
+		return result;
+	}
 }
+
+export default Handle;
